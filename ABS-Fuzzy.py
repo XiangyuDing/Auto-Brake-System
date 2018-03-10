393972638f089a9ffe866f13fcc038482fa66d46
Xiangyu Ding <xid30@pitt.edu>
2018-03-10 00:22:38 -0500

    ABS_FZ v0.2

--- ABS-Fuzzy.py
+++ ABS-Fuzzy.py
@@ -2,7 +2,7 @@ import numpy as np
 import skfuzzy as fuzz
 from skfuzzy import control as ctrl
 
-
+'''
 def ABStest():
     # Input initial variables: distance and velocity
     init_s = float(input("Current distance is(m):"))
@@ -40,54 +40,54 @@ def feedback(distance, speed):
         count += 1
     T = t * count
     print('Total time is {0:1f}'.format(T))
+'''
 
+##def FLctrl(current_distance, current_speed):
+# Define input and output variables
+distance = ctrl.Antecedent(np.arange(0, 50, 1), 'distance')
+velocity = ctrl.Antecedent(np.arange(0, 30, 1), 'velocity')
+brake = ctrl.Consequent(np.arange(0, 45, 1), 'brake')
 
-def FLctrl(current_distance, current_speed):
-    # Define input and output variables
-    distance = ctrl.Antecedent(np.arange(0, 50, 1), 'distance')
-    velocity = ctrl.Antecedent(np.arange(0, 30, 1), 'velocity')
-    brake = ctrl.Consequent(np.arange(0, 30, 1), 'brake')
+# Distance membership function
+distance['near'] = fuzz.trimf(distance.universe, [0, 0, 25])
+distance['medium'] = fuzz.trimf(distance.universe, [0, 25, 50])
+distance['far'] = fuzz.trimf(distance.universe, [25, 50, 50])
 
-    # Distance membership function
-    distance['near'] = fuzz.trimf(distance.universe, [0, 0, 25])
-    distance['medium'] = fuzz.trimf(distance.universe, [0, 25, 50])
-    distance['far'] = fuzz.trimf(distance.universe, [25, 50, 50])
+# Velocity membership function
+velocity['low'] = fuzz.trimf(velocity.universe, [0, 0, 15])
+velocity['medium'] = fuzz.trimf(velocity.universe, [0, 15, 30])
+velocity['high'] = fuzz.trimf(velocity.universe, [15, 30, 30])
 
-    # Velocity membership function
-    velocity['low'] = fuzz.trimf(velocity.universe, [0, 0, 15])
-    velocity['medium'] = fuzz.trimf(velocity.universe, [0, 15, 30])
-    velocity['high'] = fuzz.trimf(velocity.universe, [15, 30, 30])
+# Brake force membership function
+brake['low'] = fuzz.trimf(brake.universe, [0, 0, 15])
+brake['medium'] = fuzz.trimf(brake.universe, [0, 15, 30])
+brake['high'] = fuzz.trimf(brake.universe, [15, 30, 45])
+brake['full'] = fuzz.trimf(brake.universe,[30, 45, 45])
 
-    # Brake force membership function
-    brake['low'] = fuzz.trimf(brake.universe, [0, 0, 15])
-    brake['medium'] = fuzz.trimf(brake.universe, [0, 15, 30])
-    brake['high'] = fuzz.trimf(brake.universe, [15, 30, 45])
-    brake['full'] = fuzz.trimf(brake.universe,[30, 45, 45])
+# Inference Engine
+rule1 = ctrl.Rule(antecedent=(distance['medium'] & velocity['low']) |
+                                             (distance['far'] & velocity['medium']),
+                         consequent=brake['low'])
+rule2 = ctrl.Rule(antecedent=(distance['near'] & velocity['low']) |
+                                             (distance['medium'] & velocity['medium']) |
+                                             (distance['far'] & velocity['high']),
+                         consequent=brake['medium'])
+rule3 = ctrl.Rule(antecedent=(distance['medium'] & velocity['high']) |
+                                             (distance['near'] & velocity['medium']),
+                         consequent=brake['high'])
+rule4 = ctrl.Rule(antecedent=(distance['near'] & velocity['high']),
+                         consequent=brake['full'])
 
-    # Inference Engine
-    rule1 = ctrl.Rule(antecedent=(distance['medium'] & velocity['low']) |
-                                                 (distance['far'] & velocity['medium']),
-                             consequent=brake['low'])
-    rule2 = ctrl.Rule(antecedent=(distance['near'] & velocity['low']) |
-                                                 (distance['medium'] & velocity['medium']) |
-                                                 (distance['far'] & velocity['high']),
-                             consequent=brake['medium'])
-    rule3 = ctrl.Rule(antecedent=(distance['medium'] & velocity['high']) |
-                                                 (distance['near'] & velocity['medium']),
-                             consequent=brake['high'])
-    rule4 = ctrl.Rule(antecedent=(distance['near'] & velocity['high']),
-                             consequent=brake['full'])
+# Controller simulation
+ABS_ctrl = ctrl.ControlSystem([rule1, rule2, rule3, rule4])
+ABS = ctrl.ControlSystemSimulation(ABS_ctrl)
 
-    # Controller simulation
-    ABS_ctrl = ctrl.ControlSystem([rule1, rule2, rule3, rule4])
-    ABS = ctrl.ControlSystemSimulation(ABS_ctrl)
-
-    # Automatic brake processing
-    ABS.input['distance'] = current_distance
-    ABS.input['velocity'] = current_speed
-    ABS.compute()
-    # brake.view(sim = ABS)
-    return ABS.output['brake']
+# Automatic brake processing
+ABS.input['distance'] = 50
+ABS.input['velocity'] = 18
+ABS.compute()
+brake.view(sim = ABS)
+##return ABS.output['brake']
 
 
 def linear_regression(res_s, res_v):
@@ -95,6 +95,7 @@ def linear_regression(res_s, res_v):
 
 def polycurvefit(res_s, res_v):
     pass
-
+'''
 if __name__ == "__main__":
     ABStest()
+'''
